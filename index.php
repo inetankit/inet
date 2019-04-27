d939386740d642781cecc07f60b6c415b5de9926
inetankit <ankit.thapar@inetweaver.com>
2019-04-27 13:31:29 +0530

    Saving files before refreshing line endings

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 echo "<h1>HI EVENRYONE</h1>";
-
+echo "<h1>HI EVENRYONE</h1>";
+echo "<h1>HI EVENRYONE</h1>";
+echo "<h1>HI EVENRYONE</h1>";
+echo "<h1>HI EVENRYONE</h1>";
+echo "<h1>HI EVENRYONE</h1>";
+echo "<h1>HI EVENRYONE</h1>";
+echo "<h1>HI EVENRYONE</h1>";
+echo "<h1>HI EVENRYONE</h1>";
